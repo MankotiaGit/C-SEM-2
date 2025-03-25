a30c2023bbc6ab5f37c4b0ae1549eb8c35216a5d
MankotiaGit <tanishkmankotia@gmail.com>
2025-03-25 15:38:15 +0530

    Add files via upload

---
+++ Function overloading.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std;
+class Math{
+	public:
+		int add(int a, int b){
+			return a + b;
+		}
+		double add( double a, double b){
+			return a + b;
+		}
+		int add (int a, int b, int c){
+			return a + b + c;
+		}
+};
+int main()
+{
+	Math obj;
+	cout << " sum of two integers: " << obj.add(5, 10) << endl;
+	cout << " sum of two doubles: " << obj.add(5.5, 2.2) << endl;
+	cout << " sum of three integers: " << obj.add(1, 2, 3) << endl;
+return 0;
+}
