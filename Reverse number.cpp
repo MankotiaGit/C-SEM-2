be6ffbedfe86a35c662699c7edc2f28a9e637298
MankotiaGit <tanishkmankotia@gmail.com>
2025-04-06 14:49:12 +0530

    Add files via upload

---
+++ Reverse number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num, rev = 0;
+    system("cls");
+    cout << "Enter a number: ";
+    cin >> num;
+    while (num > 0) {
+        rev = rev * 10 + num % 10;
+        num /= 10;
+    }
+    cout << "Reversed Number: " << rev << endl;
+    return 0;
+}
