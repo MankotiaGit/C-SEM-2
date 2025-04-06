8d66fba21cc100a2b2841139b35cd92d6385e524
MankotiaGit <tanishkmankotia@gmail.com>
2025-04-06 11:26:31 +0530

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+    long long first = 0, second = 1, next;
+    cout << "Fibonacci Series: " << first << " " << second << " ";
+    for (int i = 3; i <= n; i++) {
+        next = first + second;
+        cout << next << " ";
+        first = second;
+        second = next;
+    }
+    cout << endl;
+    return 0;
+}
